f9b1a97aa0ac3184142d2a1bcd63ef0e64eea54b
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-07-24 15:59:59 +0300

    24.07.2023

--- src/test/java/get_requests/Get09.java
+++ src/test/java/get_requests/Get09.java
@@ -1,6 +1,17 @@
 package get_requests;
 
-public class Get09 {
+import base_urls.GoRestBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.GoRestTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get09 extends GoRestBaseUrl {
     /*
         Given
             https://gorest.co.in/public/v1/users/3810855
@@ -22,4 +33,26 @@ public class Get09 {
     }
          }
      */
+
+    @Test
+    public void test09(){
+        spec.pathParams("first","users","second","3810855");
+
+        GoRestTestData obj = new GoRestTestData();
+        Map<String,String> dataKeyMap = obj.dataKeyMap("Rep. Advaya Gowda","gowda_advaya_rep@yost.example","female","active");
+        Map<String,Object> expectedData = obj.expectedDataMethod(null,dataKeyMap);
+        System.out.println(expectedData);
+
+        Response response = given().spec(spec).when().get(  "/{first}/{second}");
+        response.prettyPrint();
+
+        Map<String,Object> actualData = response.as(HashMap.class);
+        System.out.println("actualData = " + actualData);
+        assertEquals(expectedData.get("meta"),actualData.get("meta"));
+        assertEquals(dataKeyMap.get("name"),((Map)(actualData.get("data"))).get("name"));
+        assertEquals(dataKeyMap.get("email"),((Map)(actualData.get("data"))).get("email"));
+        assertEquals(dataKeyMap.get("gender"),((Map)(actualData.get("data"))).get("gender"));
+        assertEquals(dataKeyMap.get("status"),((Map)(actualData.get("data"))).get("status"));
+        assertEquals(200,response.statusCode());
+    }
 }
