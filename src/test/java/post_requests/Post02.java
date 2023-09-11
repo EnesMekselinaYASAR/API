a48ab9f935a62e15a776f905d60e1cb8de8e81de
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-09-11 17:00:05 +0300

    11.09.2023

--- src/test/java/post_requests/Post02.java
+++ src/test/java/post_requests/Post02.java
@@ -1,62 +1,46 @@
 package post_requests;
 
-import base_urls.JsonplaceholderBaseUrl;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+import base_urls.RestfulBaseUrl;
 import org.junit.Test;
-import test_data.JsonPlaceHolderTestData;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertEquals;
-
-public class Post02 extends JsonplaceholderBaseUrl {
-
-    /*
-    Given
-      1) https://jsonplaceholder.typicode.com/todos
-      2){
-            "userId": 55,
-            "title": "Tidy your room",
-            "completed": false
-        }
-
-      When
+public class Post02 extends RestfulBaseUrl {
+     /*
+   Given
+       1) https://restful-booker.herokuapp.com/booking
+       2) {
+            "firstname": "John",
+            "lastname": "Doe",
+            "totalprice": 11111,
+            "depositpaid": true,
+            "bookingdates": {
+                "checkin": "2021-09-09",
+                "checkout": "2021-09-21"
+             }
+          }
+   When
        I send POST Request to the Url
-
-      Then
-       Status code is 201
-
-      And
-       response body is like {
-                               "userId": 55,
-                               "title": "Tidy your room",
-                               "completed": false,
-                               "id": 201
-                             }
-     */
+   Then
+       Status code is 200
+       And response body should be like
+       {
+    "bookingid": 168,
+    "booking": {
+        "firstname": "John",
+        "lastname": "Doe",
+        "totalprice": 11111,
+        "depositpaid": true,
+        "bookingdates": {
+            "checkin": "2021-09-09",
+            "checkout": "2021-09-21"
+        }
+    }
+}
+    */
 
     @Test
-    public void post01() {
-        //set the url
-        spec.pathParam("1", "todos");
-
-        //set the expected data
-        JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
-        Map<String, Object> expectedData = obj.expectedDataMethod(55, "Tidy your room", false);
-
-        //Send the post request and get the response
-        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{1}");
-        response.prettyPrint();
-
-        //do assertions
-        Map<String, Object> actualData = response.as(HashMap.class);
-
-        assertEquals(expectedData.get("completed"),actualData.get("completed"));
-        assertEquals(expectedData.get("title"),actualData.get("title"));
-        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+    public void post02(){
+        spec.pathParam("First","booking");
 
     }
+
 }
