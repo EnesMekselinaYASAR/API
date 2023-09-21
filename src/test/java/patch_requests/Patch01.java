56d9e86f2cd4a8ab32865ac97af2ffb7fbfe9a53
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-09-21 15:33:00 +0300

    21.09.2023

--- src/test/java/patch_requests/Patch01.java
+++ src/test/java/patch_requests/Patch01.java
@@ -6,9 +6,11 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import test_data.JsonPlaceHolderTestData;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Patch01 extends JsonplaceholderBaseUrl {
     /*
@@ -31,13 +33,25 @@ public class Patch01 extends JsonplaceholderBaseUrl {
 
     @Test
     public void patch01(){
+        // set the url
         spec.pathParams("first","todos","second",198);
 
+        // set the expected data
         JsonPlaceHolderTestData obj =new JsonPlaceHolderTestData();
         Map<String,Object> expectedData= obj.expectedDataMethod(null,"Wash the dishes",null);
         System.out.println("expectedData = " + expectedData);
 
+        // send the request and get the response
         Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().patch("/{first}/{second}");
         response.prettyPrint();
+
+        // do assertion
+        Map<String,Object> actualData= response.as(HashMap.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200,response.statusCode());
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+
+
     }
 }
